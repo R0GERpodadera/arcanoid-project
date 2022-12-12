0ae532add4420c20e61253061f1c081cc83b0d53
R0GERpodadera <118533396+R0GERpodadera@users.noreply.github.com>
2022-12-12 03:14:53 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ README.md
@@ -0,0 +1,64 @@
+
+> Abra esta pagina en [https://r0gerpodadera.github.io/arcanoid-project-1/](https://r0gerpodadera.github.io/arcanoid-project-1/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/r0gerpodadera/arcanoid-project-1** e importar
+
+## Edita este proyecto ![Insignia de estado de compilación](https://github.com/r0gerpodadera/arcanoid-project-1/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/r0gerpodadera/arcanoid-project-1** y haga clic en importar
+
+## Vista previa de bloques
+
+Esta imagen muestra el código de los bloques de la última confirmación en master.
+Esta imagen puede tardar unos minutos en actualizarse.
+
+![Una vista renderizada de los bloques](https://github.com/r0gerpodadera/arcanoid-project-1/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> Abra esta pagina en [https://r0gerpodadera.github.io/arcanoid-project/](https://r0gerpodadera.github.io/arcanoid-project/)
+
+## Usar como extensión
+
+Este repositorio puede ser añadido como una **extensión** en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **New Project**
+* haga clic en **Extensiones** en el menú del engranaje
+* buscar **https://github.com/r0gerpodadera/arcanoid-project** e importar
+
+## Edita este proyecto ![Insignia de estado de compilación](https://github.com/r0gerpodadera/arcanoid-project/workflows/MakeCode/badge.svg)
+
+Para editar este repositorio en MakeCode.
+
+* abra [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* haga clic en **Import** luego haga clic en **Import URL**
+* pegue **https://github.com/r0gerpodadera/arcanoid-project** y haga clic en importar
+
+## Vista previa de bloques
+
+Esta imagen muestra el código de los bloques de la última confirmación en master.
+Esta imagen puede tardar unos minutos en actualizarse.
+
+![Una vista renderizada de los bloques](https://github.com/r0gerpodadera/arcanoid-project/raw/master/.github/makecode/blocks.png)
+
+#### Metadatos (utilizados para búsqueda, renderizado)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,134 @@
+input.onButtonPressed(Button.A, function () {
+    if (barposition > 0) {
+        led.unplot(barposition, 4)
+        led.unplot(barposition + 1, 4)
+        barposition += -1
+        led.plot(barposition, 4)
+        led.plot(barposition + 1, 4)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (barposition < 3) {
+        led.unplot(barposition, 4)
+        barposition += 1
+        led.plot(barposition, 4)
+        led.plot(barposition + 1, 4)
+    }
+})
+let barposition = 0
+barposition = 2
+let ballx = randint(1, 3)
+let Bally = 0
+let trueorfalse = 100
+let random = randint(1, 2)
+let ballspeed = 500
+// codigo de la barra
+basic.forever(function () {
+    led.plot(barposition, 4)
+    led.plot(barposition + 1, 4)
+    while (trueorfalse == 150) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        Bally += 1
+        ballx += -1
+        if (Bally == 3 && (ballx == barposition || (ballx == barposition + 1 || (ballx == barposition - 1 || ballx == barposition + 1 + 0)))) {
+            trueorfalse = 0
+        }
+        if (Bally == 4) {
+            basic.showString("YOU LOST")
+        }
+    }
+    while (trueorfalse == 130) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        Bally += 1
+        ballx += 1
+        if (Bally == 3 && (ballx == barposition || (ballx == barposition + 1 || (ballx == barposition - 1 || ballx == barposition + 1 + 0)))) {
+            trueorfalse = 0
+        }
+        if (Bally == 4) {
+            basic.showString("YOU LOST")
+        }
+    }
+    while (trueorfalse == 100) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        if (random == 1) {
+            Bally += 1
+            ballx += 1
+        }
+        if (random == 2) {
+            Bally += 1
+            ballx += -1
+        }
+        if (ballx == 0) {
+            trueorfalse = 130
+        }
+        if (ballx == 4) {
+            trueorfalse = 150
+        }
+        if (Bally == 3 && (ballx == barposition || (ballx == barposition + 1 || (ballx == barposition - 1 || ballx == barposition + 1 + 0)))) {
+            trueorfalse = 0
+        }
+        if (Bally == 4) {
+            basic.showString("YOU LOST")
+        }
+    }
+    while (trueorfalse == 50) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        ballx += -1
+        Bally += -1
+        if (Bally == 0) {
+            trueorfalse = 100
+            random = randint(1, 2)
+        }
+    }
+    while (trueorfalse == 10) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        ballx += 1
+        Bally += -1
+        if (Bally == 0) {
+            trueorfalse = 100
+            random = randint(1, 2)
+        }
+    }
+    while (trueorfalse == 0) {
+        led.plot(ballx, Bally)
+        basic.pause(ballspeed)
+        ballspeed += -4
+        led.unplot(ballx, Bally)
+        if (random == 1) {
+            ballx += -1
+            Bally += -1
+        }
+        if (random == 2) {
+            ballx += 1
+            Bally += -1
+        }
+        if (Bally == 0) {
+            ballx = 1
+        }
+        if (ballx == 0) {
+            trueorfalse = 10
+        }
+        if (ballx == 4) {
+            trueorfalse = 50
+        }
+        if (Bally == 0) {
+            trueorfalse = 100
+            random = randint(1, 2)
+        }
+    }
+})
