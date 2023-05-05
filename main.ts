213e8de8c9d0e3899ae5d437519ce97e4cb937f8
R0GERpodadera <118533396+R0GERpodadera@users.noreply.github.com>
2023-05-05 13:32:42 +0200

    pre-alpha

--- main.ts
+++ main.ts
@@ -2,13 +2,26 @@ enum RadioMessage {
     message1 = 49434
 }
 radio.onReceivedNumber(function (receivedNumber) {
-    if (intro == true) {
+    if (intro == 1) {
         recibido = receivedNumber
+        radio.sendNumber(enviado)
     }
-    if (intro == false) {
-        ballx = receivedNumber
-        Bally = receivedNumber
-        balldy = receivedNumber
+    if (intro == 0) {
+        if (OrgRec == 1) {
+            ballx = receivedNumber
+        }
+        if (OrgRec == 2) {
+            Bally = receivedNumber
+        }
+        if (OrgRec == 3) {
+            balldx = receivedNumber
+            activo = 1
+            balldy = 1
+        }
+        OrgRec = OrgRec + 1
+        if (OrgRec == 4) {
+            OrgRec = 1
+        }
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -28,32 +41,36 @@ input.onButtonPressed(Button.B, function () {
         led.plot(barposition + 1, 4)
     }
 })
-let enviado = 0
-let intro = false
-let recibido = 0
+let activo = 0
+let OrgRec = 0
+let intro = 0
 let balldy = 0
+let balldx = 0
 let Bally = 0
 let ballx = 0
 let barposition = 0
+let recibido = 0
+let enviado = 0
+enviado = randint(1, 100)
+recibido = 0
 barposition = 2
 let score = 0
 ballx = 3
 Bally = 2
-let balldx = -1
+balldx = -1
 balldy = -1
 let tiempo = 500
 recibido = 0
-let activo = 2
-intro = true
+intro = 1
+OrgRec = 1
 radio.setGroup(1)
 // codigo de la barra
 basic.forever(function () {
     while (recibido == 0) {
-        enviado = randint(1, 100)
         radio.sendNumber(enviado)
         basic.pause(100)
     }
-    radio.sendNumber(enviado)
+    intro = 0
     led.plot(barposition, 4)
     led.plot(barposition + 1, 4)
     if (enviado > recibido) {
@@ -75,7 +92,8 @@ basic.forever(function () {
         if (Bally == 0 && balldy == -1) {
             radio.sendNumber(ballx)
             radio.sendNumber(Bally)
-            radio.sendNumber(balldy * -1)
+            radio.sendNumber(balldx)
+            activo = 0
         }
         if (Bally == 3 && (ballx == barposition || ballx == barposition + 1)) {
             balldy = balldy * -1
@@ -85,8 +103,5 @@ basic.forever(function () {
             basic.showString("Score")
             basic.showString("" + (score))
         }
-    }
-    if (activo == 0) {
-    	
     }
 })
